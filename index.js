afa958c4475a408a938beb30d3a5deee9745c518
vish-n-u <vishnuna26@gmail.com>
2023-10-04 16:37:51 +0530

    first commit

--- index.js
+++ index.js
@@ -14,21 +14,16 @@ const corsOptions = {
 const cookieParser = require('cookie-parser')
 const multer = require('multer'); // Middleware for handling FormData
 
-// Configure multer to handle FormData
 const storage = multer.memoryStorage(); // Store the file in memory
 const upload = multer({ storage });
 
 app.use(express.json()); // Parse JSON request bodies
 app.use(cookieParser());
 app.use(cors(corsOptions))
-// Define a route to handle the PDF file upload
 app.post('/uploadPDF', upload.single('pdfFile'), (req, res) => {
   const pdfBuffer = req.file.buffer; // Access the uploaded PDF as a Buffer
 dataExisting = pdfBuffer
-  // Process the PDF file as needed
-  // For example, you can save it to disk, send it to another service, etc.
-
-  // Send a response back to the frontend
+  
   res.status(200).send('PDF file received and processed on the server');
 });
 app.get('/getfile', (req, res) => {
